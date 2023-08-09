f174ca6e17bfa68ec42df62f5f22946c790a5dc4
haybek <haybekbatuhan@gmail.com>
2023-08-09 10:57:59 +0300

    dd

---
+++ EfCrudCalisma/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using EfCrudCalisma.Data;
+using EfCrudCalisma.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EfCrudCalisma.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SozlukContext _context; // artik bu sekilde yapilacak
+
+        //SozlukContext _db = new SozlukContext(); bu artik burada yazilmayacak
+
+        public HomeController(ILogger<HomeController> logger, SozlukContext context) // buna dependency injection deniyor
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // anasayfada rastgele bir kelime ve anlamini gosterelim
+            int adet = _context.Girdiler.Count();
+            int skip = new Random().Next(adet); // 0 - 3
+            var sozcuk = _context.Girdiler.Skip(skip).First();
+            return View(sozcuk);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EfCrudCalisma/Controllers/SozlukController.cs
@@ -0,0 +1,76 @@
+﻿using EfCrudCalisma.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCrudCalisma.Controllers
+{
+    public class SozlukController : Controller
+    {
+        private readonly SozlukContext _context;
+        public SozlukController(SozlukContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Yeni(Girdi girdi)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Girdiler.Add(girdi);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Sil(int id)
+        {
+            var girdi = _context.Girdiler.FirstOrDefault(x => x.Id == id);
+            if (girdi == null)
+            {
+                return NotFound();
+            }
+            return View(girdi);
+        }
+        [ActionName("Sil")]
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult SilOnay(int id)
+        {
+            var girdi = _context.Girdiler.FirstOrDefault(x => x.Id == id);
+            if (girdi == null)
+            {
+                return NotFound();
+            }
+            _context.Girdiler.Remove(girdi);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Duzenle(int id)
+        {
+            var girdi = _context.Girdiler.FirstOrDefault(x => x.Id == id);
+            if (girdi == null)
+            {
+                return NotFound();
+            }
+            return View(girdi);
+        }
+        
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Duzenle(Girdi girdi)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(girdi);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
